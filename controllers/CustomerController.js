4ddf4a935e8532c94dbc70b885620d5407e9fa65
tolyaganzin <tolyaganzin@mail.ru>
2016-09-08 09:14:31 +0300

    Првельная структура приложения на ангуляре

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -1,9 +1,16 @@
-angular
-    .module('app')
-    .controller('CustomerController', CustomerController);
+// моментально выполняет код сразуже прсле подключения
+(function () {
 
-    function CustomerController() {
-        var vm = this;
-        vm.title = 'Some Title';
-        vm.sendMessage = function() { };
-    }
+  // функция для 'CustomerController'
+  function CustomerController() {
+      var vm = this;
+      vm.title = 'Some Title';
+      //vm.sendMessage = function() { };
+  }
+
+  //использование модуля 'app' для 'CustomerController'
+  angular
+      .module('app')
+      .controller('CustomerController', CustomerController);
+
+})();
